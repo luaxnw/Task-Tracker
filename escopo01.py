4f701103f9d8ebb27bf939e208d1db93f1de4b3d
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-01-21 11:51:15 -0300

    Feito alterações

--- escopo01.py
+++ escopo01.py
@@ -1,18 +1,26 @@
 import argparse
 import time
+import json
+
+counter = 0
 
 curr = time.time()
 auxData = time.ctime(curr)
 
-parser = argparse.ArgumenteParser(description="Create tasks")
+parser = argparse.ArgumentParser(prog="Task-Tracker",
+                                  description="Create tasks")
 parser.add_argument("taskName", help="Give a name for a task. ", type=str)
-parser.add_argumento("description", help="give a description fot a task", type=str)
-parser.add_argument("currentData", help="Current data. ")
-
-
+parser.add_argument("description", help="give a description fot a task", type=str)
 
 args = parser.parse_args()
-args.currentData = auxData
-print(currentData)
-print(auxData)
+
+data = {"Data" : auxData,
+        "Tasnk Name" : args.taskName,
+        "Description" : args.description
+        }
+with open("task.json", "w",) as f:
+    json.dump(data,f)
+
+auxTrue = print(f"Data: {auxData} \nTask Name: {args.taskName} \nDescription: {args.description} ")
+
 
