dadace4cfbf3b1401486fb2ad97b0c1230afe5fd
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-02-27 10:13:46 -0300

    Almost finish.

--- mainProgram/task.py
+++ mainProgram/task.py
@@ -3,25 +3,25 @@ import time
 from functions import load_data, save_data, edit_task, rm_task, list_tasks
 
 parser = argparse.ArgumentParser(
-    prog="Task-Tracker",
-    description="Create and edit tasks"
+    prog="task-tracker",
+    description="create, edit, remove and list tasks"
 )
 
 subparsers = parser.add_subparsers(dest="command", required=True)
 
 
-add_parser = subparsers.add_parser("add", help="Criar nova task")
-add_parser.add_argument("taskName", type=str, help="Nome da task")
-add_parser.add_argument("description", type=str, help="Descrição da task")
+add_parser = subparsers.add_parser("add", help="create a new task")
+add_parser.add_argument("taskName", type=str, help="task's name")
+add_parser.add_argument("description", type=str, help="tasks's description")
 
 
-edit_parser = subparsers.add_parser("edit", help="Editar task existente")
-edit_parser.add_argument("id", type=int, help="ID da task")
-edit_parser.add_argument("--name", help="Novo nome da task")
-edit_parser.add_argument("--description", help="Nova descrição da task")
+edit_parser = subparsers.add_parser("edit", help="edit task's dates.")
+edit_parser.add_argument("id", type=int, help="task's ID")
+edit_parser.add_argument("--name", help="new task name")
+edit_parser.add_argument("--description", help="new task description")
 
-rm_parser = subparsers.add_parser("rm", help="Remover task")
-rm_parser.add_argument("id", type=int, help="ID da task")
+rm_parser = subparsers.add_parser("rm", help="remove task")
+rm_parser.add_argument("id", type=int, help="task's id")
 
 show_parser = subparsers.add_parser("list",help="list all tasks")
 show_parser.add_argument("show")
